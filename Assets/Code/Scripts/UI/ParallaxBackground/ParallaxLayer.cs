5d37d3be4d675446bb91616f9d7efa0ad6dd95c7
Han <147114637+hanhercales@users.noreply.github.com>
2025-09-14 11:50:36 +0700

    Parallax Background - Buổi 1

--- Assets/Code/Scripts/UI/ParallaxBackground/ParallaxLayer.cs
+++ Assets/Code/Scripts/UI/ParallaxBackground/ParallaxLayer.cs
@@ -6,10 +6,16 @@ using UnityEngine;
 public class ParallaxLayer : MonoBehaviour
 {
     public float speed;
+
+    [SerializeField] private float xLimit;
+    
+    private Vector3 originalPosition;
     private ParallaxCamera parallaxCamera;
 
     private void Start()
     {
+        originalPosition = transform.localPosition;
+        
         parallaxCamera = FindObjectOfType<ParallaxCamera>();
         if (parallaxCamera != null)
         {
@@ -22,5 +28,10 @@ public class ParallaxLayer : MonoBehaviour
         Vector3 newPosition = transform.position;
         newPosition.x += deltaMovement * speed;
         transform.position = newPosition;
+
+        if (Mathf.Abs(transform.localPosition.x) > xLimit)
+        {
+            speed *= -1;
+        }
     }
 }
